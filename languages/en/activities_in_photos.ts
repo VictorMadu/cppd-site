b34ab4e446d3abd996a147b0529c42d3d90ede21
VictorMadu <ebubevm@gmail.com>
2022-03-12 08:53:50 +0100

    Restructure and Additions
    - Moved the carousel images to activities folder
    
    - Added all text from doc

---
+++ languages/en/activities_in_photos.ts
@@ -0,0 +1,29 @@
+const activitiesInPhotos = [
+  {
+    photo: "c-1",
+    texts: [
+      "Crowning of the resolution of the land dispute between Emeora Neke Community and Umuezeogbu Umualor Community of Enugu State of Nigeria, espoused by CPPD-Nigeria, was the signing of peace agreement between the two communities as a laid down accord bequeathed their future generations to ensure lasting peace and brotherly co-existence.",
+      "Above, on November 13, 2008, His Royal Highness, Igwe Silas Udogu (of Emeora) and Igwe Hyacinth Ugwu (of Umuezeogbu) sign peace agreement, while the Executive Director and Executive Secretary of CPPD (in black suits) observe. ",
+    ],
+  },
+  {
+    photo: "c-2",
+    texts: [
+      "The Chairman of Isi-uzo Local Government, Dr Sam Ugwu, congratulates Dr. J.C. Madu, the Executive Director of CPPD, for the CPPD’s roles in resolving land conflict and working out peace between Emeora Neke Community and Umuezeogbu Umualor Community of Isi-Uzo Local Government Area.",
+    ],
+  },
+  {
+    photo: "c-3",
+    texts: [
+      "A member of CPPD, Dr Madu (5th from right, front row), participated in 2008 peace research training at the United Nations Headquarters in Addis Ababa, courtesy of the UPEACE Africa Programme and IDRC.",
+    ],
+  },
+  {
+    photo: "c-4",
+    texts: [
+      "Executive Director of CPPD receives UPEACE-IDRC 2009 Doctoral Research Award.",
+    ],
+  },
+];
+
+export default activitiesInPhotos;
--- languages/en/organization.ts
+++ languages/en/organization.ts
@@ -10,5 +10,127 @@ const organzation = {
       numbers: ["(0)8038129140", "(0)803321862", "(0)806456445"],
     },
   },
+  structure: [
+    {
+      rank: "Board of Trustees",
+      desc: "The highest policy-making body, headed by the chairman.",
+    },
+    {
+      rank: "Council of Advisers",
+      desc:
+        "Group of eminent persons in and outside Nigeria, who are renowned in their professions and in CPPD’s fields of operation and who support CPPD with their wealth of knowledge and advice.",
+    },
+    {
+      rank: "Management",
+      desc:
+        "The organisation is run by team of all unit heads, the Executive Secretary, and the Executive Director who heads the management.",
+    },
+    {
+      rank: "Members",
+      desc:
+        "These are made up of researchers of different hierarchy, development professionals, academics, peace activists, policy makers, etc.",
+    },
+    {
+      rank: "Staff",
+      desc:
+        "Those employed to work for the organization, including our coordinators of CBOs at the grassroots level (in all local government areas of our host states).",
+    },
+  ],
+  vision:
+    "This Organization envisions a humane society where people can live in security and dignity, free from poverty and uncertainty.",
+  mission: {
+    achieve_peace_and_dev:
+      "The mission of the CENTRE FOR PROMOTION OF PEACE AND DEVELOPMENT is to bridge the gap between research and policy in order to achieve peace and development through:",
+    achieve_peace_and_dev_list: [
+      "Embarking on and advancing multi-sectoral researches",
+      "Carrying out human security and development projects",
+      "Carrying out conflict resolution and peace actions",
+      "Training and capacity building",
+      "Engaging in equity and human rights concerns",
+      "Embarking on advocacy, policy debates and dialogues",
+      "Embarking on consultancy services",
+    ],
+    above_achived_through:
+      "The above objectives are planned and being carried out through the following means: ",
+    above_achived_through_list: [
+      {
+        title: "Research",
+        texts: {
+          text1:
+            "Research is an important guide to development actions; hence, it is one of the major primary tools CPPD uses in its work to solve social problems as well as for project identification and designing interventions. Apart from embarking on and advancing multi-sectoral researches, the organization supports its individual members in their researches that are of high quality and that can impact positively on policy and development. The following are our research areas for which funds are solicited to support (See the description of research areas)",
+          list1: [
+            "Gender and Rural Development",
+            "Human Rights",
+            "Governance, Democratization and Public Policy",
+            "Public Health",
+            "Peace and Conflicts ",
+          ],
+          text2:
+            "We are also encouraging high quality of research in Nigerian universities and hope to introduce best research award for higher degree thesis and dissertations in social sciences and humanities, especially in the field of development, policy, human rights, environment, conflict and peace, economic growth and governance",
+        },
+      },
+      {
+        title:
+          "Human security projects – addressing violence,  rights issues, health, development and poverty reduction projects, educational needs, etc., ",
+        texts: {
+          text1:
+            "Safety is the hallmark of freedom from fear, violence and ignorance while well being is the target of freedom from want, anxiety, deprivation and disease. We are not only concerned with threats of violence but also physical threats like poverty, disease, illiteracy, economic, political and civil rights.",
+          list1: [
+            {
+              title: "Poverty Reduction",
+              text:
+                "Also, as a grassroots social organisation, with attracted funds, CPPD embarks on proactive development projects aimed at reducing poverty, empowering the rural people and the unemployed population through skill training and income generating activities. We support concrete cooperative projects that link communities in mutually beneficial undertaking because these enhance the cooperation and confidence of these communities for durable peace. CPPD embarks on empowering rural women (especially the less privileged and widows) through credit, skill acquisition and provision of safe homes. Depending on funding capacity, we support women, village cooperatives and associations in carrying out poverty reduction projects.",
+            },
+            {
+              title: "Environment  and Public Health",
+              texts: [
+                "Providing poor people with basic health and nutrition is at the heart of reducing poverty and promoting economic growth. Our health programme is to fast track high impact interventions in the following areas: maternal and child health, reproductive health, environmental sanitation, immunization, combating malaria and HIV/AIDS, health education as well as rural health for the less privileged. We are committed to seeing that female gender and children in Nigerian society are protected and have access to good health and resources.",
+                "Concern for the environment is an important part of poverty reduction strategy since environment is very crucial to poor people in terms of their health, security, earning capacity, physical surroundings, peace of mind, as well as their access to and control of environmental services and resources. Therefore, CPPD’s attention also focuses on environmental assessments and safeguard policies. The greatest costs of biodiversity loss are felt by rural people in developing countries, who are the most dependent on it for food, shelter, medicine, income, employment, and cultural identity. It is CPPD’s effort to contribute in better protection of biodiversity hotspots of the rural communities.",
+              ],
+            },
+            {
+              title: "Justice and Human rights",
+              texts: [
+                "Justice is a human need while access to security, justice, economic growth and development are basic human rights. We have justice/legal unit. Ours is a difficult society where many people, especially the poor, suffer injustice and abuse of their rights because of certain circumstances or their social status. We are committed to addressing inequities and defending the vulnerable (especially women and children). Women face a lot of challenges in Nigeria due to culture, religion patriarchal practice, conflicts and discrimination.",
+                "Hence, we offer FREE legal advice and representation for the disadvantaged and the vulnerable in Nigeria. We also challenge unjust structures and actions that undermine the lives and rights of the citizens.",
+                "Our objectives include meeting specific needs in human rights advocacy and activism.  The issue of human rights and policing in Nigerian context is of much concern to CPPD. We are interested in carrying out researches and interventions to help in strengthening the community policing in Nigeria, service provision to communities by informal policing, as well as improving police-public relations. CPPD supports and promotes human rights and laws for pro-poor justice sector reform, to take care of the weak and vulnerable groups in Nigeria.",
+              ],
+            },
+            {
+              title: "Education",
+              texts: [
+                "As emphasised in its 7th Principle, the Declaration of the Rights of the Child Proclaimed by UN General Assembly resolution 1386(XIV) of 20 November 1959: “The child is entitled to receive education, which shall be free and compulsory, at least in the elementary stages. He shall be given an education which will promote his general culture and enable him, on a basis of equal opportunity, to develop his abilities, his individual judgment, and his sense of moral and social responsibility, and to become a useful member of society.”",
+                "Though much has been said by governments of Nigeria about the importance of education, this has remained a lip service and this results to constant strikes in the nation’s educational institutions, poor quality of education and hopeless future for the society. Depending on the availability of fund, CPPD is committed to affording the child quality education irrespective of gender, to help achieve the Millennium Development Goal on education through scholarships, grants and good nutrition.",
+              ],
+            },
+            { title: "", text: "" },
+          ],
+        },
+      },
+      {
+        title: "Advocacy and policy dialogues",
+        texts: [
+          "Weak and inappropriate policies as well as inept governance impede reforms and development in Nigeria – making human security and attainment of even the first Millennium Development Goal by this country a mirage. Advocacy and policy dialogues are carried out to address areas of weakness or problems in government policies.",
+          "Through communication and outreach this organization hopes to increase Nigerians’ awareness of their rights. By using research, policy debates and dialogues, inter-agency and state-civil society cooperation as well as broad sector activities, we can make those in government to be more accountable to the people they serve.",
+          "Messages are to be tailored to meet the need of the target audience and our communication system will have ability to create focus and interest groups capable of making their voices heard.  A greater awareness of problems and policies will always prompt debate and can encourage and initiate solutions conducive to the creation of a sustained and sustainable peace and development.",
+          "There is provision for free legal advice and representation for the disadvantaged group, while we support alternative and non-violent dispute resolution.",
+        ],
+      },
+      {
+        title: "Justice, conflict resolution and peace action",
+        texts: [
+          "The Centre for Promotion of Peace and Development (CPPD) Nigeria is playing important role in Nigeria as there cannot be an enduring peace without a viable democratic system and socio-economic development. Peace and Conflict Resolution Unit conducts peace researches and carries out resolution of conflicts among people and in the communities where conflicts occur. In response to the United Nations mandate for the International Decade for a Culture of Peace and Non-violence, CPPD promotes the principle of non-violence and initiative for reconciliation among Nigerian communities.",
+          "Through its activities and sponsored programmes, CPPD intends to build the culture of non-violence in Nigerian campuses and also support the creation of strategies to alleviate the plight of children that face violence. Activities are undertaken and support is given for prevention, resolution and management of conflict by formal and informal means, but especially the latter.",
+        ],
+      },
+      {
+        title: "Consultancy",
+        texts: [
+          "Many projects fail due to poor identification, planning, appraisal, design and implementation. Using our expert staff, we offer assistance through consultancy services to identify community needs (projects), guide investment decision and project implementation for success. We help in preparation of good project proposals and feasibility studies, as well as in performance improvement.",
+          "Many organizations, including the public service sectors, often have problem of achieving desired goals, efficiency and effectiveness in the implementation of available resources. CPPD renders performance audit for performance improvement in organizations and departments, helping to design and implement required intervention and solve performance problems.",
+        ],
+      },
+    ],
+  },
 };
 export default organzation;

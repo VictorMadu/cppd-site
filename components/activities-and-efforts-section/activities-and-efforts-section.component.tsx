970bd321197fbe5c01fdf6bed3a436981bde206e
VictorMadu <ebubevm@gmail.com>
2022-03-17 11:48:12 +0100

    Refactored ActivitiesAndEffortsComponent and used MissionContent component in it.

--- components/activities-and-efforts-section/activities-and-efforts-section.component.tsx
+++ components/activities-and-efforts-section/activities-and-efforts-section.component.tsx
@@ -1,24 +1,15 @@
 import React from "react";
 import * as app from "../../languages/en/app";
 import activitiesAndEfforts from "../../languages/en/texts/activities_and_efforts";
-import map from "lodash/map";
 import * as styles from "./activities-and-efforts-section.styles";
 import { IProps } from "./activities-and-efforts-section.interface";
+import MissionContent from "../about_section/mission_content/mission_content.component";
 
 const ActivitesAndEffortsSection = (props: IProps) => {
   return (
     <div className={styles.container(props)}>
       <p className={styles.title(props)}>{app.activitiesAndEfforts}</p>
-      <p className={styles.paragraph(props)}>{activitiesAndEfforts.text1}</p>
-      {map(activitiesAndEfforts.texts2, (aes, index) => (
-        <div key={index} className={styles.paragraph(props)}>
-          {map(aes, (ae, index) => (
-            <p key={index} className="bg-red-200 mt-1">
-              {ae}
-            </p>
-          ))}
-        </div>
-      ))}
+      <MissionContent contents={activitiesAndEfforts.contents} />
     </div>
   );
 };

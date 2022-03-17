15f1469cae6a6414372ddc490ce1d2812ba94336
VictorMadu <ebubevm@gmail.com>
2022-03-17 11:46:23 +0100

    Refactored PreambleComponent and used MissionContent component in it.

--- components/preamble-section/preamble-section.component.tsx
+++ components/preamble-section/preamble-section.component.tsx
@@ -2,6 +2,7 @@ import { map } from "lodash";
 import React from "react";
 import * as app from "../../languages/en/app";
 import preamble from "../../languages/en/texts/preamble";
+import MissionContent from "../about_section/mission_content/mission_content.component";
 import { IProps } from "./preamble-section.interface";
 import * as styles from "./preamble-section.styles";
 
@@ -10,11 +11,7 @@ const PreambleSection = (props: IProps) => {
     <div className={styles.container(props)}>
       <p className={styles.title(props)}>{app.preamble}</p>
       <div className={styles.textContainer(props)}>
-        {map(preamble.texts, (text, index) => (
-          <p key={index} className={styles.paragraph(props)}>
-            {text}
-          </p>
-        ))}
+        <MissionContent contents={preamble.contents} />
       </div>
       <div className={styles.authorDescContainer(props)}>
         <p className={styles.authorName(props)}>{preamble.author.name}</p>
--- languages/en/texts/preamble.ts
+++ languages/en/texts/preamble.ts
@@ -1,17 +1,37 @@
-const preamble = {
-  title: "preamble",
-  texts: [
-    `Research has been an important guide to development actions, but it is not an end in itself. Hence, research should not be limited in studying human misery without seeking to alleviate it. Weak and inappropriate policies as well as inept governance impede reforms and development in Nigeria – making human security and attainment of different development goals a mirage in this country.`,
+import { IContainer } from "./texts.interface";
 
-    ` Our country would continue to degrade in conflicts, economic and social ruins if we do not learn how to live together and if we fail to articulate solutions for the challenging situations. Though there could be outside assistance, building better governance has to come from within each country, which needs commitment of the people. Hence, the Centre for Promotion of Peace and Development (CPPD), Nigeria was formed to assist in building a strong political, social and economic order in Nigeria. `,
-    `
-     CPPD is incorporated in Nigeria as a research and development non-governmental organization, which is independent and non-profit making. It combines a network of researchers, development professionals, academics and policy makers in various sectors. The organisation strives to realise the values, principles and goals of the United Nations Millennium Summit Declaration: peace, security, poverty eradication, democracy, good governance, human rights, development, protecting the vulnerable and meeting the special needs of our country, Nigeria. I am happy to introduce this organization, its activities and key members.`,
+export const preamble = {
+  title: "preamble",
+  contents: [
+    {
+      text:
+        "Research has been an important guide to development actions, but it is not an end in itself. Hence, research should not be limited in studying human misery without seeking to alleviate it. Weak and inappropriate policies as well as inept governance impede reforms and development in Nigeria – making human security and attainment of different development goals a mirage in this country.",
+      type: "para",
+    },
+    {
+      text:
+        "Our country would continue to degrade in conflicts, economic and social ruins if we do not learn how to live together and if we fail to articulate solutions for the challenging situations. Though there could be outside assistance, building better governance has to come from within each country, which needs commitment of the people. Hence, the Centre for Promotion of Peace and Development (CPPD), Nigeria was formed to assist in building a strong political, social and economic order in Nigeria.",
+      type: "para",
+    },
+    {
+      text:
+        "CPPD is incorporated in Nigeria as a research and development non-governmental organization, which is independent and non-profit making. It combines a network of researchers, development professionals, academics and policy makers in various sectors. The organisation strives to realise the values, principles and goals of the United Nations Millennium Summit Declaration: peace, security, poverty eradication, democracy, good governance, human rights, development, protecting the vulnerable and meeting the special needs of our country, Nigeria. I am happy to introduce this organization, its activities and key members.",
+      type: "para",
+    },
   ],
   author: {
     name: "J. C. Madu",
     position: "Executive Director",
     date: "1st January 2009",
   },
+} as {
+  title: string;
+  contents: IContainer;
+  author: {
+    name: string;
+    position: string;
+    date: string;
+  };
 };
 
 export default preamble;

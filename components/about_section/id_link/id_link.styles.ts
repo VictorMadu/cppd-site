e611da0d7e8f14930e999f588d2616078f6ec787
VictorMadu <ebubevm@gmail.com>
2022-03-19 07:55:22 +0100

    Removed unnecessary comment

--- components/about_section/id_link/id_link.styles.ts
+++ components/about_section/id_link/id_link.styles.ts
@@ -12,8 +12,4 @@ export const link = (props: IProps) =>
     "uppercase",
     "text-base",
     "text-slate-900/60 hover:text-pri-900/80 focus:text-pri-900/80"
-    // "underline-offset-4",
-    // "hover:decoration-2 focus:decoration-2",
-    // "hover:decoration-pri-900/80 focus:decoration-pri-900/80",
-    // "hover:underline focus:underline"
   );
